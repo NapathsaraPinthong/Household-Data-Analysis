cee905bad66e052bf86f3b73ae44ff3fad38a8a1
Napathsara Pinthong <napathsara.pin@gmail.com>
2024-08-23 20:05:14 +0700

    feat: fg-level add aggregate function
    commit
    
    feat: fg-level add aggregate function

---
+++ data/fragile-level.py
@@ -0,0 +1,89 @@
+from connection import client
+import pandas as pd
+
+try:
+    db = client['MSO']
+    hh_collection = db['Household']
+    mb_collection = db['Member']
+    
+    pipeline = [
+        {
+            '$match': {
+                'status': {
+                    '$ne': 'removed'
+                }
+            }
+        }, {
+            '$lookup': {
+                'from': 'Member', 
+                'localField': '_id', 
+                'foreignField': 'hh_id', 
+                'as': 'members'
+            }
+        }, {
+            '$match': {
+                'members': {
+                    '$ne': []
+                }
+            }
+        }, {
+            '$unwind': {
+                'path': '$members', 
+                'preserveNullAndEmptyArrays': True
+            }
+        }, {
+            '$match': {
+                'members.status': {
+                    '$ne': 'removed'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0, 
+                'hh_id': '$_id', 
+                'mb_id': '$members._id', 
+                'age': '$members.age', 
+                'sum_income': '$members.SUM_INCOME', 
+                'is_disabled': '$members.is_disabled', 
+                'prob_family': {
+                    '$map': {
+                        'input': '$members.prob_family', 
+                        'as': 'item', 
+                        'in': {
+                            'code': '$$item.code'
+                        }
+                    }
+                }, 
+                'prob_health': {
+                    '$map': {
+                        'input': '$members.prob_health', 
+                        'as': 'item', 
+                        'in': {
+                            'code': '$$item.code'
+                        }
+                    }
+                }
+            }
+        }
+    ]
+    
+    # Execute the aggregation pipeline
+    results = list(hh_collection.aggregate(pipeline))
+    df = pd.DataFrame(results)
+    
+    """""
+    Use 'df' to be input of calculating function 
+    
+    """""
+    
+
+    #Save the DataFrame to an Excel file
+    # file_path = 'output.xlsx'
+    # df.to_excel(file_path, index=False)
+    # print(f"DataFrame saved to {file_path}") 
+    
+
+
+finally:
+    # Ensure the connection is closed even if an error occurs
+    client.close()
