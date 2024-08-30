eabd97ba26113ff5e4ee73b4b55585fca8a2f555
Napathsara Pinthong <napathsara.pin@gmail.com>
2024-08-30 17:26:15 +0700

    feat: clean repo and add function total income

---
+++ data/data_repository.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+
+# Check if member is dependent based on the given criteria
+def is_dependent(member):
+    is_child = 0 <= member['age'] <= 18
+    is_single_parent = False
+    is_senior = False  
+    is_disabled = member.get('is_disabled', 0) == 1
+    is_patient = False
+
+    if isinstance(member['prob_family'], list):
+        is_single_parent = any(code in ['PBFA14', 'PBFA25'] for code in [item.get('code') for item in member['prob_family']])
+
+    if isinstance(member['prob_health'], list):
+        is_patient = any(code in ['PBHE4', 'PBHE8'] for code in [item.get('code') for item in member['prob_health']])
+
+    if 'age' in member:
+        is_senior = member['age'] >= 60
+
+    return is_child or is_single_parent or is_senior or is_disabled or is_patient
+
+# Determine fragile level based on criteria
+def determine_fg_level(row):
+    if row['total_income'] >= 100000 and row['num_dependents'] >= 1:
+        return 0
+    elif row['total_income'] < 100000 and row['num_dependents'] == 0:
+        return 1
+    elif row['total_income'] < 100000 and 1 <= row['num_dependents'] <= 2:
+        return 2
+    elif row['total_income'] < 100000 and row['num_dependents'] > 2:
+        return 3
+    else:
+        return -1  # Just in case there's an unexpected situation
+
+def export_hh_total_income(df):
+    household_summary = df.groupby('hh_id').agg(
+        total_income=pd.NamedAgg(column='sum_income', aggfunc='sum')
+    ).reset_index()
+    
+    df_summary = df.merge(household_summary[['hh_id', 'total_income']], on='hh_id', how='left')
+    export_df = df_summary[['hh_id', 'total_income']].copy()
+    return export_df
+    
+
+def export_hh_fg_level(df, path_name):
+    # Calculate if each member is dependent
+    df['is_dependent'] = df.apply(is_dependent, axis=1)
+    # Aggregate household income and count dependents for each household
+    household_summary = df.groupby('hh_id').agg(
+        total_income=pd.NamedAgg(column='sum_income', aggfunc='sum'),
+        num_dependents=pd.NamedAgg(column='is_dependent', aggfunc='sum')
+    ).reset_index()
+    household_summary['fg_level'] = household_summary.apply(determine_fg_level, axis=1)
+    # Merge the fg_level back to the original DataFrame
+    df = df.merge(household_summary[['hh_id', 'fg_level']], on='hh_id', how='left')
+    
+    # Prepare DataFrame for export with member ID and income level
+    export_df = df[['hh_id', 'fg_level']].copy()
+    # Export to Excel without header
+    export_df.to_excel(path_name, index=False, header=False)
