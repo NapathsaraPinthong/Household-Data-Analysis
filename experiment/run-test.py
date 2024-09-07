0d63acc6b4dd8aab5afa455246ee3c8201d122c1
Pavida Malitong <pavida2545@gmail.com>
2024-09-07 17:35:09 +0700

    feat: improve experiment function on fragile level
    commit

--- experiment/run-test.py
+++ experiment/run-test.py
@@ -10,18 +10,27 @@ sampled_hh_ids = load_sampled_households('./static/sampled_households.csv')
 embedding_dict = create_embedding_dict(embedding_df)
 hh_member_dict = load_hh_member_dict('./static/hh_member_dict.pkl')
 
-# -------- Usage -------- #
-# distance_lv_minus_1 = calculate_distance_in_fragile_level(-1, embedding_df)
-# distance_lv0 = calculate_distance_in_fragile_level(0, embedding_df)
-# distance_lv1 = calculate_distance_in_fragile_level(1, embedding_df)
-# distance_lv2 = calculate_distance_in_fragile_level(2, embedding_df)
-# distance_lv3 = calculate_distance_in_fragile_level(3, embedding_df)
+# -------- Calculate Distance for Fragile Level -------- #
+province_codes = [40, 50, 90]
+get_sample_group_with_embeddings_and_fg_level(province_codes, embedding_df)
 
-# print(f"Level -1: {distance_lv_minus_1}")
-# print(f"Level 0: {distance_lv0}")
-# print(f"Level 1: {distance_lv1}")
-# print(f"Level 2: {distance_lv2}")
-# print(f"Level 3: {distance_lv3}")
+distance_lv_minus_1 = calculate_distance_in_fragile_level(-1)
+distance_lv0 = calculate_distance_in_fragile_level(0)
+distance_lv1 = calculate_distance_in_fragile_level(1)
+distance_lv2 = calculate_distance_in_fragile_level(2)
+distance_lv3 = calculate_distance_in_fragile_level(3)
 
+print(f"Level -1: {distance_lv_minus_1}")
+print(f"Level 0: {distance_lv0}")
+print(f"Level 1: {distance_lv1}")
+print(f"Level 2: {distance_lv2}")
+print(f"Level 3: {distance_lv3}")
+
+centroid_distances = calculate_all_centroid_distances()
+print("Distances between centroids of all fragile levels:")
+print(centroid_distances)
+
+
+# -------- Calculate Distance for Household Property -------- #
 overall_avg_distance = calculate_distance_for_households(sampled_hh_ids, embedding_dict, hh_member_dict)
 print(f"Overall Average Distance: {overall_avg_distance}")
