9f53031cb139609b4e666362589e7db455f1492a
Pavida Malitong <pavida2545@gmail.com>
2024-09-15 16:24:09 +0700

    feat: improve household test

--- experiment/run-test.py
+++ experiment/run-test.py
@@ -2,31 +2,26 @@ import pandas as pd
 from fragile_level_utils import *
 from hh_mb_utils import *
 
-file_path = "../main/result/node_embeddings_10_5.xlsx"
+file_path = "../main/result/node_embeddings_50_25.xlsx"
 column_names = ["node_id", "value", "node_target"]
-embedding_df = pd.read_excel(file_path, header=None, names=column_names)
+embedding_df = pd.read_excel(file_path, header=0, names=column_names)
+
+sampled_hh_ids = load_sampled_households('./static/sampled_households.csv')
+embedding_dict = create_embedding_dict(embedding_df)
+hh_member_dict = load_hh_member_dict('./static/hh_member_dict.pkl')
 
 # -------- Usage -------- #
-distance_lv_minus_1 = calculate_distance_in_fragile_level(-1, embedding_df)
-distance_lv0 = calculate_distance_in_fragile_level(0, embedding_df)
-distance_lv1 = calculate_distance_in_fragile_level(1, embedding_df)
-distance_lv2 = calculate_distance_in_fragile_level(2, embedding_df)
-distance_lv3 = calculate_distance_in_fragile_level(3, embedding_df)
+# distance_lv_minus_1 = calculate_distance_in_fragile_level(-1, embedding_df)
+# distance_lv0 = calculate_distance_in_fragile_level(0, embedding_df)
+# distance_lv1 = calculate_distance_in_fragile_level(1, embedding_df)
+# distance_lv2 = calculate_distance_in_fragile_level(2, embedding_df)
+# distance_lv3 = calculate_distance_in_fragile_level(3, embedding_df)
 
-print(f"Level -1: {distance_lv_minus_1}")
-print(f"Level 0: {distance_lv0}")
-print(f"Level 1: {distance_lv1}")
-print(f"Level 2: {distance_lv2}")
-print(f"Level 3: {distance_lv3}")
+# print(f"Level -1: {distance_lv_minus_1}")
+# print(f"Level 0: {distance_lv0}")
+# print(f"Level 1: {distance_lv1}")
+# print(f"Level 2: {distance_lv2}")
+# print(f"Level 3: {distance_lv3}")
 
-distance_hh1 = calculate_distance_in_household(5244898460, embedding_df)
-distance_hh2 = calculate_distance_in_household(8501888293, embedding_df)
-distance_hh3 = calculate_distance_in_household(1873160590, embedding_df)
-distance_hh4 = calculate_distance_in_household(5719322689, embedding_df)
-distance_hh5 = calculate_distance_in_household(8478917799, embedding_df)
-
-print(f"House 1: {distance_hh1}")
-print(f"House 2: {distance_hh2}")
-print(f"House 3: {distance_hh3}")
-print(f"House 4: {distance_hh4}")
-print(f"House 5: {distance_hh5}")
+overall_avg_distance = calculate_distance_for_households(sampled_hh_ids, embedding_dict, hh_member_dict)
+print(f"Overall Average Distance: {overall_avg_distance}")
